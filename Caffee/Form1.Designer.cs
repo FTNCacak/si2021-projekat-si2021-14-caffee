e4f3d54b09abf3069aa5fc812d44fc049c41b6d6
uncle-draza <78-2019@ftn.edu.rs>
2021-12-13 10:40:27 +0100

    dodat naslov i ikonica na title bar 7/7

--- Caffee/Form1.Designer.cs
+++ Caffee/Form1.Designer.cs
@@ -32,16 +32,21 @@ namespace Caffee
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.panel_Header = new System.Windows.Forms.Panel();
-            this.button_close = new System.Windows.Forms.Button();
-            this.button_windowed = new System.Windows.Forms.Button();
             this.button_minimize = new System.Windows.Forms.Button();
+            this.button_windowed = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox_titlebar_logo = new System.Windows.Forms.PictureBox();
+            this.label_title = new System.Windows.Forms.Label();
             this.panel_Header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_titlebar_logo)).BeginInit();
             this.SuspendLayout();
             // 
             // panel_Header
             // 
             this.panel_Header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(44)))));
+            this.panel_Header.Controls.Add(this.label_title);
+            this.panel_Header.Controls.Add(this.pictureBox_titlebar_logo);
             this.panel_Header.Controls.Add(this.button_minimize);
             this.panel_Header.Controls.Add(this.button_windowed);
             this.panel_Header.Controls.Add(this.button_close);
@@ -50,6 +55,39 @@ namespace Caffee
             this.panel_Header.Name = "panel_Header";
             this.panel_Header.Size = new System.Drawing.Size(1206, 32);
             this.panel_Header.TabIndex = 0;
+            // 
+            // button_minimize
+            // 
+            this.button_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_minimize.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_minimize.BackgroundImage")));
+            this.button_minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_minimize.FlatAppearance.BorderSize = 0;
+            this.button_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minimize.Font = new System.Drawing.Font("Microsoft Tai Le", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_minimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.button_minimize.Location = new System.Drawing.Point(1068, 0);
+            this.button_minimize.Name = "button_minimize";
+            this.button_minimize.Size = new System.Drawing.Size(46, 32);
+            this.button_minimize.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.button_minimize, "Minimize");
+            this.button_minimize.UseVisualStyleBackColor = true;
+            this.button_minimize.Click += new System.EventHandler(this.button_minimize_Click);
+            // 
+            // button_windowed
+            // 
+            this.button_windowed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_windowed.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_windowed.BackgroundImage")));
+            this.button_windowed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_windowed.FlatAppearance.BorderSize = 0;
+            this.button_windowed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_windowed.Font = new System.Drawing.Font("Microsoft Tai Le", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_windowed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.button_windowed.Location = new System.Drawing.Point(1114, 0);
+            this.button_windowed.Name = "button_windowed";
+            this.button_windowed.Size = new System.Drawing.Size(46, 32);
+            this.button_windowed.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.button_windowed, "Restore Down");
+            this.button_windowed.UseVisualStyleBackColor = true;
             // 
             // button_close
             // 
@@ -69,38 +107,26 @@ namespace Caffee
             this.button_close.UseVisualStyleBackColor = true;
             this.button_close.Click += new System.EventHandler(this.button_close_Click);
             // 
-            // button_windowed
+            // pictureBox_titlebar_logo
             // 
-            this.button_windowed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_windowed.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_windowed.BackgroundImage")));
-            this.button_windowed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button_windowed.FlatAppearance.BorderSize = 0;
-            this.button_windowed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_windowed.Font = new System.Drawing.Font("Microsoft Tai Le", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_windowed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
-            this.button_windowed.Location = new System.Drawing.Point(1114, 0);
-            this.button_windowed.Name = "button_windowed";
-            this.button_windowed.Size = new System.Drawing.Size(46, 32);
-            this.button_windowed.TabIndex = 2;
-            this.toolTip1.SetToolTip(this.button_windowed, "Restore Down");
-            this.button_windowed.UseVisualStyleBackColor = true;
+            this.pictureBox_titlebar_logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_titlebar_logo.BackgroundImage")));
+            this.pictureBox_titlebar_logo.Location = new System.Drawing.Point(6, 0);
+            this.pictureBox_titlebar_logo.Name = "pictureBox_titlebar_logo";
+            this.pictureBox_titlebar_logo.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox_titlebar_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox_titlebar_logo.TabIndex = 1;
+            this.pictureBox_titlebar_logo.TabStop = false;
             // 
-            // button_minimize
+            // label_title
             // 
-            this.button_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_minimize.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_minimize.BackgroundImage")));
-            this.button_minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button_minimize.FlatAppearance.BorderSize = 0;
-            this.button_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_minimize.Font = new System.Drawing.Font("Microsoft Tai Le", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_minimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
-            this.button_minimize.Location = new System.Drawing.Point(1068, 0);
-            this.button_minimize.Name = "button_minimize";
-            this.button_minimize.Size = new System.Drawing.Size(46, 32);
-            this.button_minimize.TabIndex = 3;
-            this.toolTip1.SetToolTip(this.button_minimize, "Minimize");
-            this.button_minimize.UseVisualStyleBackColor = true;
-            this.button_minimize.Click += new System.EventHandler(this.button_minimize_Click);
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.label_title.Location = new System.Drawing.Point(40, 6);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(85, 20);
+            this.label_title.TabIndex = 4;
+            this.label_title.Text = "MyCaffee";
             // 
             // MainWindow
             // 
@@ -113,6 +139,8 @@ namespace Caffee
             this.Text = "MyCaffee";
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.panel_Header.ResumeLayout(false);
+            this.panel_Header.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_titlebar_logo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -124,6 +152,8 @@ namespace Caffee
         private System.Windows.Forms.Button button_windowed;
         private System.Windows.Forms.Button button_minimize;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox_titlebar_logo;
+        private System.Windows.Forms.Label label_title;
     }
 }
 
