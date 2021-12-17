13f6afba2b011ba5aa3944552e4e7faf012639ea
Branislav Radojevic <branislavradojevicit4@gmail.com>
2021-12-18 00:57:48 +0100

    Dodati podaci u bazu, i kreirana Constants klasa

---
+++ CaffeeData/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeeData
+{
+    public class Constants
+    {
+        public string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CaffeeDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    }
+}
