83513009a0352e02798bdffa322f4786be92e14f
Pavle Brnovic <50-2019@ftn.edu.rs>
2021-12-29 21:53:38 +0100

    Kreiran biznis sloj i klase na njemu

---
+++ CaffeBusiness/OrderBsiness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaffeeData;
+using CaffeeData.Models;
+
+namespace CaffeBusiness
+{
+    public class OrderBsiness
+    {
+        //instance of OrderRepository that allows us to access methods of data layer
+        OrderRepository orderRepository = new OrderRepository();    
+
+        //method that calls GetAllOrder from OrderRepository class and returns list of all orders
+        List<Order> getAllOrders()
+        {
+            List<Order> orders = orderRepository.GetAllOrders();
+            return orders;
+        }
+        //method that pases order object to InsertObject method on dataLayer
+        bool insertOrder(Order o)
+        {
+            return orderRepository.InsertOrder(o)!=0;
+        }
+    }
+}
---
+++ CaffeBusiness/OrderItemBusiness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaffeeData;
+using CaffeeData.Models;
+namespace CaffeBusiness
+{
+    public class OrderItemBusiness
+    {
+        //instance of OrderItemRepository class that allows us to access data layer
+        private readonly OrderItemRepository orderItemRepository = new OrderItemRepository();
+        
+        //method that calls GetAllOrderItems from OrderItemRepository class and returns list of all OrderedItems
+
+        public  List<OrderItem> getAllOrderItems()
+        {
+           List<OrderItem> orderItems = orderItemRepository.GetAllOrderItems();
+            return orderItems;
+        }
+        //method that calls insertOrderItem from OrderItemRepository class and returns true if number of affected rows is !=0
+        public bool insertOrderItem(OrderItem o)
+        {
+            return orderItemRepository.InsertOrderItem(o) != 0;
+        }
+    }
+}
