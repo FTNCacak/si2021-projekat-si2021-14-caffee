a45beafb30d40020d89856d3f3e0a3c8e411b5a2
uncle-draza <dilpdrazovic@gmail.com>
2021-12-30 17:59:13 +0100

    dodat datagridview u menu settings

---
+++ Caffee/MenuSettings.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Caffee
+{
+    partial class MenuSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuSettings));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel_Header = new System.Windows.Forms.Panel();
+            this.label_title = new System.Windows.Forms.Label();
+            this.pictureBox_titlebar_logo = new System.Windows.Forms.PictureBox();
+            this.button_close = new System.Windows.Forms.Button();
+            this.dataGridView_menuItems = new System.Windows.Forms.DataGridView();
+            this.ItemName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel_Header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_titlebar_logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_menuItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_Header
+            // 
+            this.panel_Header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(44)))));
+            this.panel_Header.Controls.Add(this.label_title);
+            this.panel_Header.Controls.Add(this.pictureBox_titlebar_logo);
+            this.panel_Header.Controls.Add(this.button_close);
+            this.panel_Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_Header.Location = new System.Drawing.Point(0, 0);
+            this.panel_Header.Name = "panel_Header";
+            this.panel_Header.Size = new System.Drawing.Size(772, 32);
+            this.panel_Header.TabIndex = 1;
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.label_title.Location = new System.Drawing.Point(40, 6);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(125, 20);
+            this.label_title.TabIndex = 4;
+            this.label_title.Text = "Menu Settings";
+            // 
+            // pictureBox_titlebar_logo
+            // 
+            this.pictureBox_titlebar_logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_titlebar_logo.BackgroundImage")));
+            this.pictureBox_titlebar_logo.Location = new System.Drawing.Point(6, 0);
+            this.pictureBox_titlebar_logo.Name = "pictureBox_titlebar_logo";
+            this.pictureBox_titlebar_logo.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox_titlebar_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox_titlebar_logo.TabIndex = 1;
+            this.pictureBox_titlebar_logo.TabStop = false;
+            // 
+            // button_close
+            // 
+            this.button_close.AccessibleDescription = "";
+            this.button_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_close.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_close.BackgroundImage")));
+            this.button_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_close.FlatAppearance.BorderSize = 0;
+            this.button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_close.Font = new System.Drawing.Font("Microsoft Tai Le", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_close.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.button_close.Location = new System.Drawing.Point(726, 0);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(46, 32);
+            this.button_close.TabIndex = 1;
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // dataGridView_menuItems
+            // 
+            this.dataGridView_menuItems.AllowUserToDeleteRows = false;
+            this.dataGridView_menuItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_menuItems.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.dataGridView_menuItems.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(44)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(204)))), ((int)(((byte)(178)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(44)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView_menuItems.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView_menuItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_menuItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ItemName,
+            this.Price});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(44)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(204)))), ((int)(((byte)(178)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(44)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView_menuItems.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView_menuItems.EnableHeadersVisualStyles = false;
+            this.dataGridView_menuItems.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView_menuItems.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView_menuItems.Name = "dataGridView_menuItems";
+            this.dataGridView_menuItems.ReadOnly = true;
+            this.dataGridView_menuItems.RowHeadersVisible = false;
+            this.dataGridView_menuItems.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_menuItems.Size = new System.Drawing.Size(748, 226);
+            this.dataGridView_menuItems.TabIndex = 2;
+            // 
+            // ItemName
+            // 
+            this.ItemName.FillWeight = 297.3656F;
+            this.ItemName.HeaderText = "Item Name";
+            this.ItemName.Name = "ItemName";
+            this.ItemName.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.FillWeight = 53.41898F;
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // MenuSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(184)))), ((int)(((byte)(146)))));
+            this.ClientSize = new System.Drawing.Size(772, 444);
+            this.Controls.Add(this.dataGridView_menuItems);
+            this.Controls.Add(this.panel_Header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MenuSettings";
+            this.Text = "MenuSettings";
+            this.panel_Header.ResumeLayout(false);
+            this.panel_Header.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_titlebar_logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_menuItems)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Header;
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.PictureBox pictureBox_titlebar_logo;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.DataGridView dataGridView_menuItems;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+    }
+}
---
+++ Caffee/MenuSettings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caffee
+{
+    public partial class MenuSettings : Form
+    {
+        public MenuSettings()
+        {
+            InitializeComponent();
+            InitializeColumns();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InitializeColumns()
+        {
+            /*dataGridView_menuItems.ColumnCount = 2;
+            dataGridView_menuItems.Columns[0].Name = "Item name";
+            dataGridView_menuItems.Columns[1].Name = "Price";*/
+
+            //temporary test data for design purpose
+            string[] row = new string[] { "Kokakola 0.33l", "75.00" };
+            dataGridView_menuItems.Rows.Add(row);
+            row = new string[] { "Lav pivo 1l", "100.00" };
+            dataGridView_menuItems.Rows.Add(row);
+            row = new string[] { "Domaca kafa", "50.00" };
+            dataGridView_menuItems.Rows.Add(row);
+            row = new string[] { "Lavita sok", "60.00" };
+            dataGridView_menuItems.Rows.Add(row);
+            row = new string[] { "Kisela voda", "30.00" };
+            dataGridView_menuItems.Rows.Add(row);
+            //temporary test data for design purpose
+
+            DataGridViewButtonColumn btnDelete = new DataGridViewButtonColumn();
+            btnDelete.HeaderText = "";
+            btnDelete.Text = "-";
+            btnDelete.Name = "btn";
+            btnDelete.FillWeight = 12;
+            btnDelete.UseColumnTextForButtonValue = true;
+            dataGridView_menuItems.Columns.Add(btnDelete);
+        }
+
+        private void dataGridView_menuItems_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            e.CellStyle.Font = new Font(e.CellStyle.Font, FontStyle.Bold);
+            e.CellStyle.SelectionBackColor = Color.FromArgb(255, 230, 204, 178);
+            e.CellStyle.SelectionForeColor = Color.FromArgb(255, 99, 65, 44);
+        }
+
+        
+    }
+}
