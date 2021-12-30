c5e8116d226640769c0edc8865904288689db566
RadojevicBranislav <72319334+RadojevicBranislav@users.noreply.github.com>
2021-12-30 18:05:06 +0100

    Dodate klase korisnik i vlasnik i funkcionalnost unosa korisnika i njegovo povlacenje iz txt fajla

---
+++ CaffeeData/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using CaffeeData.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeeData
+{
+    public class UserRepository
+    {
+        public List<User> GetAllUsers()
+        {
+            //Nisam mogao da pronadjem resenje za cuvanje u folderu u 
+            string filePath = @"C:\Users\Bane\Desktop\ProjekatSi\si2021-projekat-si2021-14-caffee\CaffeeData\Users.txt";
+
+            List<string> lines = new List<string>();
+
+            List<User> users = new List<User>();
+
+            lines = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in lines)
+            {
+                string[] items = line.Split(',');
+
+                User u = new User(items[0], items[1], Convert.ToBoolean(items[2]));
+
+                users.Add(u);
+            }
+
+            return users;
+        }
+
+        public void InsertNewUser(User u)
+        {
+            string filePath = @"C:\Users\Bane\Desktop\ProjekatSi\si2021-projekat-si2021-14-caffee\CaffeeData\Users.txt";
+
+            List<User> users = GetAllUsers();
+
+            users.Add(u);
+
+            List<string> outContents = new List<string>();
+
+            foreach (User user in users)
+            {
+                outContents.Add(user.ToString());
+            }
+
+            File.WriteAllText(filePath, "");
+
+            File.WriteAllLines(filePath, outContents);
+        }
+
+    }
+}
