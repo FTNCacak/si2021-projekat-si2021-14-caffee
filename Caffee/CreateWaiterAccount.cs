cd0c54b308e7a70442b02b94a010ebcca3df29b3
uncle-draza <78-2019@ftn.edu.rs>
2022-01-01 18:13:56 +0100

    kreirane sve login forme i info/warning/error dialog forma

---
+++ Caffee/CreateWaiterAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caffee
+{
+    public partial class CreateWaiterAccount : Form
+    {
+        public CreateWaiterAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonCreateOwnerAcc_Click(object sender, EventArgs e)
+        {
+            //upisivanje u fajl za konobare
+            //prikazivanje infoboxa da je uspesno kreiran konobar i pitanje da li zelimo da dodamo jos (yes no dialog)
+            //yes --> ciscenje textboxova i dodavanje jos jednog konobara
+            //no --> prelazak na login formu
+            //ciscenje texboxova
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            //gasenje ove forme i prelazak na login formu
+            var lgn = new Login();
+            lgn.Closed += (s, args) => this.Close();
+            this.Hide();
+            lgn.Show();
+        }
+    }
+}
